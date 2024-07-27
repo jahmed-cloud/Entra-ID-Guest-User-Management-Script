0e1284a12b1576d3c49b9c0fe814e2bd964356df
jahmed-cloud <jahmed.cloud@outlook.com>
2024-07-28 01:54:35 +0530

    updated

--- Azure Test/Basics/basic.ps1
+++ Azure Test/Basics/basic.ps1
@@ -8,24 +8,3 @@ $Credential = New-Object -TypeName System.Management.Automation.PSCredential `
 -ArgumentList $ClientID,$SecureSecret
 
 Connect-AzAccount -ServicePrincipal -Credential $Credential -Tenant $TenantID
-
-# create Azure Resource Group
-
-$RGroupName = "Powershell-rg"
-$RGroupLocation = "Central India"
-
-$ResoruceGroup = New-AzResourceGroup -Name $RGroupName -Location $RGroupLocation
-
-"Provisioning state" + $ResoruceGroup.ProvisioningState
-
-$ResoruceGroup
-
-$ExisitingResourceGroup = Get-AzResourceGroup -Name $RGroupName
-
-$AllResourceGroup = Get-AzResourceGroup
-
-foreach($Group in $AllResourceGroup)
-{
-    'Removing Resource Group' + $Group.ResourceGroupName
-    Remove-AzResourceGroup -Name $Group.ResourceGroupName -Force
-}
